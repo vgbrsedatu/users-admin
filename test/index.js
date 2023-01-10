46a7153df1ae06e10229d42785a986a0fa62a5de
Victor Giovanni Beltrán Rodríguez <vgbr.sedatu@gmail.com>
2023-01-10 00:05:46 -0600

    test: add unit test files

---
+++ test/index.js
@@ -0,0 +1,18 @@
+const { describe, it } = require('node:test');
+const assert = require('node:assert');
+
+describe('A thing', () => {
+  it('should work', () => {
+    assert.strictEqual(1, 1);
+  });
+
+  it('should be ok', () => {
+    assert.strictEqual(2, 2);
+  });
+
+  describe('a nested thing', () => {
+    it('should work', () => {
+      assert.strictEqual(3, 3);
+    });
+  });
+});
