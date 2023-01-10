2393e61575c807c0de5c43dcaa451d625a3ff763
Victor Giovanni Beltrán Rodríguez <vgbr.sedatu@gmail.com>
2023-01-10 00:12:22 -0600

    feat: add notification system module

---
+++ electron/system/notification/index.js
@@ -0,0 +1,53 @@
+/**
+ * @author Victor Giovanni Beltrán Rodríguez.
+ * @file Manage electron notification settings.
+ */
+
+// ━━ IMPORT MODULES ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+// » IMPORT ELECTRON APIS
+import { Notification } from 'electron';
+
+// ━━ CONSTANTS ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+/**
+ * The `LEVELS` Object contains all urgency level of the notification.
+ *
+ * @private
+ * @constant {object} levels
+ */
+const levels = {
+  0: 'low',
+  1: 'normal',
+  2: 'critical',
+};
+
+// ━━ MAIN MODULES ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+/**
+ * The `notification()` function that show a native notification on macOS, Windows
+ * and Linux.
+ *
+ * @private
+ * @param   {object} options - Adds information to notification.
+ * @param   {string} options.title - Representing the title of the notification.
+ * @param   {string} options.body - Body notification.
+ * @param   {string} options.icon - An icon to use in the notification.
+ * @param   {number} options.urgency - The urgency level of the notification.
+ * @returns {Notification} Launch a notification.
+ * @example notification({
+ *   title: 'A test notification',
+ *   body: 'I am a body notification',
+ *   urgency: 'critical',
+ *   icon: 'assets/icons/app.ico',
+ * });
+ *
+ */
+const notification = ({ title, body, icon, urgency }) =>
+  new Notification({
+    title,
+    body,
+    icon,
+    urgency: levels[urgency] || levels[0],
+    silent: false,
+  });
+
+// ━━ EXPORT MODULE ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+export default notification; // eslint-disable-line import/prefer-default-export
