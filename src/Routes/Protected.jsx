66e9c3f1c4655a37bafccd478498257878674050
Victor Giovanni Beltrán Rodríguez <vgbr.sedatu@gmail.com>
2023-01-12 22:51:31 -0600

    feat: add protected component

---
+++ src/Routes/Protected.jsx
@@ -0,0 +1,42 @@
+/**
+ * @author Victor Giovanni Beltrán Rodríguez
+ * @file Manage `Protected` React component.
+ */
+
+// ━━ IMPORT MODULES ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+// » IMPORT REACT MODULES
+import { useLocation, Navigate } from 'react-router-dom';
+import { PropTypes } from 'prop-types';
+
+// » IMPORT APP COMPONENT
+import { useAuthConsumer } from '../Context/AuthContext';
+import Loading from '../Components/Loading';
+
+// ━━ COMPONENT ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+/**
+ * The `Protected` component, provides protection for some routes if there is
+ * no authentication.
+ *
+ * @component
+ * @returns {JSX.Element} The `Protected` components.
+ */
+const Protected = ({ children }) => {
+  const { auth, loading } = useAuthConsumer();
+  const { pathname } = useLocation();
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (!auth) {
+    return <Navigate to="/signin" state={{ from: pathname }} replace />;
+  }
+  return children;
+};
+
+Protected.propTypes = {
+  children: PropTypes.element.isRequired,
+};
+
+// ━━ EXPORT MODULE ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+export default Protected;
