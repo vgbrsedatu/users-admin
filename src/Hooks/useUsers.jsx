6dc33613d8541490674f9146326c4721ae09c381
Victor Giovanni Beltrán Rodríguez <vgbr.sedatu@gmail.com>
2023-01-12 23:10:43 -0600

    docs: add jsdoc comments

--- src/Hooks/useUsers.jsx
+++ src/Hooks/useUsers.jsx
@@ -12,11 +12,38 @@ import { getUsers } from '../services/firebase/api/users';
 
 // ━━ TYPE DEFINITIONS ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 /**
- * The returns value from `useOpacity`
+ * An `object` with user information.
  *
- * @typedef   {object}    OpacityResponse
- * @property  {string}    opacity         - The `opacity` value.
- * @property  {(value:string) => void}  changeOpacity   - A function to change `opacity` state.
+ * @typedef   {object}  User
+ * @property  {string}  email                 - User email.
+ * @property  {string}  name                  - User name.
+ * @property  {string}  password              - User password.
+ * @property  {string}  phone                 - User phone.
+ * @property  {boolean} emailVerified         - User emailVerified.
+ * @property  {boolean} disabled              - User disabled.
+ * @property  {string}  role                  - User role.
+ * @property  {object}  company               - Information about User company.
+ * @property  {string}  company.name          - User company name.
+ * @property  {string}  company.department    - User company department.
+ * @property  {string}  company.title         - User company title.
+ * @property  {string}  company.location      - User company location.
+ * @property  {object}  address               - Information about user address.
+ * @property  {string}  address.street        - User address street.
+ * @property  {string}  address.number        - User address number.
+ * @property  {string}  address.settlement    - User address settlement.
+ * @property  {string}  address.postal        - User address postal.
+ * @property  {string}  address.municipality  - User address municipality.
+ * @property  {string}  address.locality      - User address locality.
+ * @property  {string}  address.state         - User address state.
+ */
+
+/**
+ * The returns value from `useUsers`
+ *
+ * @typedef   {object}        UsersResponse
+ * @property  {boolean}       loading       - The initial value is `false`, it changes when the `useUsers` hook finishes loading the `users`.
+ * @property  {boolean|Error} error         - The initial value is false, it changes if there was a problem loading users.
+ * @property  {Array.<User>}  Users         - The collection of users.
  */
 
 // ━━ CUSTOM REACT HOOK ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
@@ -24,12 +51,12 @@ import { getUsers } from '../services/firebase/api/users';
  * The `useOpacity` it's a custom React hook witch communicates with the
  * `electron` api, used to manages opacity state.
  *
- * @returns {OpacityResponse} An object to manage the opacity state.
+ * @returns {UsersResponse} An object to manage the opacity state.
  */
 const useUsers = () => {
   const [users, setUsers] = useState();
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(true);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     getUsers()
