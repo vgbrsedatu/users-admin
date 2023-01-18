fc486c1a0b48a6668e89f3682275a232589aaee3
Victor Giovanni Beltrán Rodríguez <vgbr.sedatu@gmail.com>
2023-01-17 20:23:06 -0600

    refactor: rename file

--- src/Hooks/useLocalStorage.jsx
+++ src/Hooks/useLocalStorage.jsx
@@ -1,6 +1,6 @@
 /**
  * @author Victor Giovanni Beltrán Rodríguez
- * @file Contains `useStorage` a custom React Hook.
+ * @file Contains `useLocalStorage` a custom React Hook.
  */
 
 // ━━ IMPORT MODULES ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
@@ -9,28 +9,62 @@ import { useState } from 'react';
 
 // ━━ TYPE DEFINITIONS ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 /**
- * The returns value from `useTheme`
+ * The `getItem()` function that returns that key's value, or `null` if the key
+ * does not exist, in the given Storage object.
  *
- * @typedef   {Map}                  StorageResponse
- * @property  {'dark'|'light'}       StorageResponse.0  - The current value of `theme`.
- * @property  {() => void}           StorageResponse.1      - A function to change the value of `theme`.
+ * @typedef {(key: string) => string|null} getItem
  */
+
+/**
+ * The `setItem()` function of the `Storage` interface, when passed a key name
+ * and value, will add that key to the given `Storage` object, or update that
+ * key's value if it already exists.
+ *
+ * @typedef {(key: string, value: *) => void} setItem
+ */
+
+/**
+ * Functions to manage the `localStorage` api.
+ *
+ * @typedef   {object}  storage
+ * @property  {getItem} Storage.getItem - The current value of `theme`.
+ * @property  {setItem} Storage.setItem - A function to change the value of `theme`.
+ */
+
+/**
+ * The current state value.
+ *
+ * @typedef {string|null} store
+ */
+
+/**
+ * Update the state value.
+ *
+ * @typedef   {(string) => void}  setValue
+ */
+
+/**
+ * The returns value from `useLocalStorage`
+ *
+ * @typedef {[store, setValue]} localStorageHook
+ */
+
 // ━━ FUNCTIONS ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
 /**
  * An object with functions to manage the `localStorage` api.
  *
- * @constant {Object} storage
+ * @type {storage} storage
  */
 const storage = {
-  get: key => {
+  getItem: key => {
     try {
       return JSON.parse(window.localStorage.getItem(key || null));
     } catch (e) {
       return null;
     }
   },
-
-  set: (key, value) => {
+  setItem: (key, value) => {
     try {
       window.localStorage.setItem(key, JSON.stringify(value));
     } catch (error) {
@@ -51,30 +85,33 @@ const isFunction = value => ({}.toString.call(value) === '[object Function]');
 
 // ━━ CUSTOM REACT HOOK ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 /**
- * The `useStorage` it's a custom React hook, manage and store a state using
- * the `localStorage` api. The `useStorage` returns an array for elements to
- * hold a value assigned with the `key` parameter.
+ * The `useLocalStorage` it's a custom React hook, manage and store a state
+ * using the `localStorage` api. The `useLocalStorage` returns an array for
+ * elements to hold a value assigned with the `key` parameter.
  *
  * The first element of the array is the current value of the state, and the
  * second is a function to change its value.
  *
  * @param {string} key - The name of the key you want to create/update.
  * @param {*} defaults - The default value.
- * @returns {[any, (value:any) => void]} An array with two element the `state` and `setState`.
+ * @returns {localStorageHook} An array to manage the `localStorage` api.
+ * @example const [opacity, setOpacity] = useLocalStorage('opacity', 1);
+ *
  */
-const useStorage = (key, defaults) => {
+const useLocalStorage = (key, defaults) => {
   const [store, setStore] = useState(() => {
-    const item = storage.get(key);
+    const item = storage.getItem(key);
     return item || defaults;
   });
 
   const setValue = value => {
     const updated = isFunction(value) ? value(store) : value;
     setStore(updated);
-    storage.set(key, updated);
+    storage.setItem(key, updated);
   };
+
   return [store, setValue];
 };
 
 // ━━ EXPORT MODULE ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-export default useStorage;
+export default useLocalStorage;
