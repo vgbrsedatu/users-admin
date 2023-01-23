5a7896eee39e04a388b5a568c2be52cd0f525958
Victor Giovanni Beltrán Rodríguez <vgbr.sedatu@gmail.com>
2023-01-22 19:22:18 -0600

    style: edit jsdoc comment

--- src/Hooks/useUser/actions.jsx
+++ src/Hooks/useUser/actions.jsx
@@ -41,6 +41,9 @@ const SET_LOADING = payload => ({
 /**
  * Auxiliary function for the reducer.
  *
+ * Update the `error` property in the `state`, the `payload` parameter must
+ * be `string`.
+ *
  * @param {object} payload
  * @param {string} payload.type - Type of action to be executed in the reducer.
  * @param {string} payload.payload - The new value that the state will take.
@@ -52,6 +55,9 @@ const SET_ERROR = payload => ({
 
 /**
  * Auxiliary function for the reducer.
+ *
+ * Update the `created` property in the `state`, the `payload` parameter must
+ * be `boolean`.
  *
  * @param {object} payload
  * @param {string} payload.type - Type of action to be executed in the reducer.
@@ -65,6 +71,9 @@ const SET_CREATED = payload => ({
 /**
  * Auxiliary function for the reducer.
  *
+ * Update the `updated` property in the `state`, the `payload` parameter must
+ * be `boolean`.
+ *
  * @param {object} payload
  * @param {string} payload.type - Type of action to be executed in the reducer.
  * @param {boolean} payload.payload - The new value that the state will take.
@@ -76,6 +85,9 @@ const SET_UPDATED = payload => ({
 
 /**
  * Auxiliary function for the reducer.
+ *
+ * Update the `deleted` property in the `state`, the `payload` parameter must
+ * be `boolean`.
  *
  * @param {object} payload
  * @param {string} payload.type - Type of action to be executed in the reducer.
@@ -89,6 +101,9 @@ const SET_DELETED = payload => ({
 /**
  * Auxiliary function for the reducer.
  *
+ * Update the `user` property in the `state`, the `payload` parameter must
+ * be `Object`.
+ *
  * @param {object} payload
  * @param {string} payload.type - Type of action to be executed in the reducer.
  * @param {object} payload.payload - The new value that the state will take.
@@ -100,6 +115,9 @@ const SET_USER = payload => ({
 
 /**
  * Auxiliary function for the reducer.
+ *
+ * Update the `name` property in the `state`, the `payload` parameter must be
+ * `string`.
  *
  * @param {object} payload
  * @param {string} payload.type - Type of action to be executed in the reducer.
@@ -113,6 +131,9 @@ const SET_NAME = payload => ({
 /**
  * Auxiliary function for the reducer.
  *
+ * Update the `email` property in the `state`, the `payload` parameter must be
+ * `string`.
+ *
  * @param {object} payload
  * @param {string} payload.type - Type of action to be executed in the reducer.
  * @param {string} payload.payload - The new value that the state will take.
@@ -124,6 +145,9 @@ const SET_EMAIL = payload => ({
 
 /**
  * Auxiliary function for the reducer.
+ *
+ * Update the `password` property in the `state`, the `payload` parameter must
+ * be `string`.
  *
  * @param {object} payload
  * @param {string} payload.type - Type of action to be executed in the reducer.
@@ -137,6 +161,9 @@ const SET_PASSWORD = payload => ({
 /**
  * Auxiliary function for the reducer.
  *
+ * Update the `phone` property in the `state`, the `payload` parameter must be
+ * `string`.
+ *
  * @param {object} payload
  * @param {string} payload.type - Type of action to be executed in the reducer.
  * @param {string} payload.payload - The new value that the state will take.
@@ -148,6 +175,9 @@ const SET_PHONE = payload => ({
 
 /**
  * Auxiliary function for the reducer.
+ *
+ * Update the `photo` property in the `state`, the `payload` parameter must be
+ * `string`.
  *
  * @param {object} payload
  * @param {string} payload.type - Type of action to be executed in the reducer.
@@ -161,6 +191,9 @@ const SET_PHOTO = payload => ({
 /**
  * Auxiliary function for the reducer.
  *
+ * Update the `verified` property in the `state`, the `payload` parameter must
+ * be `boolean`.
+ *
  * @param {object} payload
  * @param {string} payload.type - Type of action to be executed in the reducer.
  * @param {boolean} payload.payload - The new value that the state will take.
@@ -172,6 +205,9 @@ const SET_VERIFIED = payload => ({
 
 /**
  * Auxiliary function for the reducer.
+ *
+ * Update the `disabled` property in the `state`, the `payload` parameter must
+ * be `boolean`.
  *
  * @param {object} payload
  * @param {string} payload.type - Type of action to be executed in the reducer.
@@ -185,6 +221,9 @@ const SET_DISABLED = payload => ({
 /**
  * Auxiliary function for the reducer.
  *
+ * Update the `role` property in the `state`, the `payload` parameter must be
+ * `string`.
+ *
  * @param {object} payload
  * @param {string} payload.type - Type of action to be executed in the reducer.
  * @param {string} payload.payload - The new value that the state will take.
@@ -196,6 +235,9 @@ const SET_ROLE = payload => ({
 
 /**
  * Auxiliary function for the reducer.
+ *
+ * Update the `company.name` property in the `state`, the `payload` parameter
+ * must be `string`.
  *
  * @param {object} payload
  * @param {string} payload.type - Type of action to be executed in the reducer.
@@ -209,6 +251,9 @@ const SET_COMPANY_NAME = payload => ({
 /**
  * Auxiliary function for the reducer.
  *
+ * Update the `company.title` property in the `state`, the `payload` parameter
+ * must be `string`.
+ *
  * @param {object} payload
  * @param {string} payload.type - Type of action to be executed in the reducer.
  * @param {string} payload.payload - The new value that the state will take.
@@ -220,6 +265,9 @@ const SET_COMPANY_TITLE = payload => ({
 
 /**
  * Auxiliary function for the reducer.
+ *
+ * Update the `company.location` property in the `state`, the `payload`
+ * parameter must be `string`.
  *
  * @param {object} payload
  * @param {string} payload.type - Type of action to be executed in the reducer.
@@ -233,6 +281,9 @@ const SET_COMPANY_LOCATION = payload => ({
 /**
  * Auxiliary function for the reducer.
  *
+ * Update the `company.department` property in the `state`, the `payload`
+ * parameter must be `string`.
+ *
  * @param {object} payload
  * @param {string} payload.type - Type of action to be executed in the reducer.
  * @param {string} payload.payload - The new value that the state will take.
@@ -244,6 +295,9 @@ const SET_COMPANY_DEPARTMENT = payload => ({
 
 /**
  * Auxiliary function for the reducer.
+ *
+ * Update the `error` property in the `state`, the `payload` parameter must
+ * be `string`.
  *
  * @param {object} payload
  * @param {string} payload.type - Type of action to be executed in the reducer.
@@ -257,6 +311,9 @@ const SET_ADDRESS_STREET = payload => ({
 /**
  * Auxiliary function for the reducer.
  *
+ * Update the `address.number` property in the `state`, the `payload`
+ * parameter must be `string`.
+ *
  * @param {object} payload
  * @param {string} payload.type - Type of action to be executed in the reducer.
  * @param {string} payload.payload - The new value that the state will take.
@@ -268,6 +325,9 @@ const SET_ADDRESS_NUMBER = payload => ({
 
 /**
  * Auxiliary function for the reducer.
+ *
+ * Update the `address.settlement` property in the `state`, the `payload`
+ * parameter must be `string`.
  *
  * @param {object} payload
  * @param {string} payload.type - Type of action to be executed in the reducer.
@@ -281,6 +341,9 @@ const SET_ADDRESS_SETTLEMENT = payload => ({
 /**
  * Auxiliary function for the reducer.
  *
+ * Update the `address.postal` property in the `state`, the `payload` parameter
+ * must be `string`.
+ *
  * @param {object} payload
  * @param {string} payload.type - Type of action to be executed in the reducer.
  * @param {string} payload.payload - The new value that the state will take.
@@ -292,6 +355,9 @@ const SET_ADDRESS_POSTAL = payload => ({
 
 /**
  * Auxiliary function for the reducer.
+ *
+ * Update the `address.municipality` property in the `state`, the `payload`
+ * parameter must be `string`.
  *
  * @param {object} payload
  * @param {string} payload.type - Type of action to be executed in the reducer.
@@ -305,6 +371,9 @@ const SET_ADDRESS_MUNICIPALITY = payload => ({
 /**
  * Auxiliary function for the reducer.
  *
+ * Update the `address.locality` property in the `state`, the `payload`
+ * parameter must be `string`.
+ *
  * @param {object} payload
  * @param {string} payload.type - Type of action to be executed in the reducer.
  * @param {string} payload.payload - The new value that the state will take.
@@ -316,6 +385,9 @@ const SET_ADDRESS_LOCALITY = payload => ({
 
 /**
  * Auxiliary function for the reducer.
+ *
+ * Update the `address.state` property in the `state`, the `payload` parameter
+ * must be `string`.
  *
  * @param {object} payload
  * @param {string} payload.type - Type of action to be executed in the reducer.
